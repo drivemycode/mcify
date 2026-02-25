c224d445b86f48696182316ca960a2eb48240212
longzhe7 <justin.long@mail.utoronto.ca>
2026-02-24 19:28:06 -0500

    basic irl pic-> mosaic

--- basic.py
+++ basic.py
@@ -3,7 +3,7 @@ from PIL import Image
 import numpy as np
 
 MAP_SIZE = (512, 512)
-MINECRAFT_WOOL_RGB = {
+MINECRAFT_BLOCKS = {
     "white_wool": (233, 236, 236),
     "orange_wool": (240, 118, 19),
     "magenta_wool": (189, 68, 179),
@@ -20,6 +20,22 @@ MINECRAFT_WOOL_RGB = {
     "green_wool": (84, 109, 27),
     "red_wool": (161, 39, 34),
     "black_wool": (20, 21, 25),
+    "white_concrete": (207, 213, 214),
+    "orange_concrete": (224, 97, 0),
+    "magenta_concrete": (169, 48, 159),
+    "light_blue_concrete": (36, 137, 199),
+    "yellow_concrete": (241, 175, 21),
+    "lime_concrete": (94, 168, 24),
+    "pink_concrete": (214, 101, 143),
+    "gray_concrete": (54, 57, 61),
+    "light_gray_concrete": (125, 125, 115),
+    "cyan_concrete": (21, 119, 136),
+    "purple_concrete": (100, 32, 156),
+    "blue_concrete": (44, 46, 143),
+    "brown_concrete": (96, 59, 31),
+    "green_concrete": (73, 91, 36),
+    "red_concrete": (142, 32, 32),
+    "black_concrete": (8, 10, 15),
 }
 
 def pixelate(img_path, pixel_size):
@@ -37,6 +53,44 @@ def pixelate(img_path, pixel_size):
 
     cv2.destroyAllWindows()
 
-if __name__ == "__main__":
+def build_palette(blocks_dict):
+    names = list(blocks_dict.keys())
+    rgb = np.array([blocks_dict[n] for n in names], dtype=np.uint8)
 
-    pixelate("room.jpeg", 8)
+    lab = cv2.cvtColor(rgb.reshape(1, -1, 3), cv2.COLOR_RGB2LAB)
+    lab = lab.reshape(-1, 3).astype(np.int16)
+
+    return names, lab
+
+def match_blocks(image_path, blocks_dict, out_size=(512, 512)):
+    img = Image.open(image_path).convert("RGB").resize(out_size, Image.NEAREST)
+    rgb_img = np.array(img, dtype=np.uint8)
+
+    lab_img = cv2.cvtColor(rgb_img, cv2.COLOR_RGB2LAB).astype(np.int16)
+    names, lab_palette = build_palette(blocks_dict)
+
+    # for every pixel, for every block in lab palette, the 3 number 
+    # vector difference
+    diff = lab_img[:, :, None, :] - lab_palette[None, None, :, :]
+    
+    # diff has shape: [y, x, K, 3].
+    # y:= height pixels
+    # x:= width pixels
+    # K:= lab color palette blocks: (lightness, green-red, blue-yellow). (0-100)
+    dist2 = np.sum(diff * diff, axis = 3) 
+    best = np.argmin(dist2, axis=2)  # axis=2 is the layer [wool1, wool2, .. woolK]
+
+    grid = np.vectorize(lambda i: names[i])(best)
+    return grid, best
+
+def preview_from_indices(indices, blocks_dict):
+    names = list(blocks_dict.keys())
+    palette_rgb = np.array([blocks_dict[n] for n in names], dtype=np.uint8)
+    return Image.fromarray(palette_rgb[indices])
+
+if __name__ == "__main__":
+    # grid, idx = match_blocks("gym.png", MINECRAFT_BLOCKS)
+    grid, idx = match_blocks("room.jpeg", MINECRAFT_BLOCKS)
+
+    preview_img = preview_from_indices(idx, MINECRAFT_BLOCKS)
+    preview_img.show("preview.png")
